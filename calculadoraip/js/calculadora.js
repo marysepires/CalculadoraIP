75557fada4d91e7c17cc8466e0955cc76727fc41
Maryse Pires <74427494+marysepires@users.noreply.github.com>
2022-07-08 16:17:11 -0300

    Update calculadora.js

--- calculadoraip/js/calculadora.js
+++ calculadoraip/js/calculadora.js
@@ -16,12 +16,6 @@ function validarEntrada(value) {
 
   value = value.trim();
 
-  if (!ehValidoMascaraOrIP(value)) {
-    throw new Error(
-      "Utilize pontos para separar os valores e não deixe espaços"
-    );
-  }
-
   return value;
 }
 
@@ -59,4 +53,57 @@ function tipoClasse(octeto) {
   } else if (octeto.slice(0, 4) === "1111") {
     return "E";
   }
+}
+
+
+/* 
+    Operação OR entre IP e o NOT da Máscara
+*/
+
+
+function converterDecimalParaBinarioQuatroOctetos(value) {
+  value = value.split(".");
+  let octetos = preencherOcteto(converterDecimalParaBinario(value[0]));
+  octetos += "." + preencherOcteto(converterDecimalParaBinario(value[1]));
+  octetos += "." + preencherOcteto(converterDecimalParaBinario(value[2]));
+  octetos += "." + preencherOcteto(converterDecimalParaBinario(value[3]));
+  return octetos;
+}
+
+function converterBinarioParaDecimal(value) {
+  return parseInt(value, 2);
+}
+
+function converteBinarioParaDecimalQuatroNumeros(value) {
+  value = value.split(".");
+  let numeros = converterBinarioParaDecimal(value[0]);
+  numeros += "." + converterBinarioParaDecimal(value[1]);
+  numeros += "." + converterBinarioParaDecimal(value[2]);
+  numeros += "." + converterBinarioParaDecimal(value[3]);
+  return numeros;
+}
+
+function negacaoBinaria(value) {
+  return value === "1" ? "0" : "1";
+}
+
+function negacaoBinariaQuatroOctetos(value) {
+  let valueNegado = "";
+  for (let i = 0; i < value.length; i++) {
+    let valueBin = value.charAt(i);
+    if (valueBin === ".") {
+      valueNegado += valueBin;
+    } else {
+      valueNegado += negacaoBinaria(valueBin);
+    }
+  }
+  return valueNegado;
+}
+
+function formatarBinarioQuatroOctetos(value) {
+  let octeto = value.slice(0, 8);
+  octeto += "." + value.slice(8, 16);
+  octeto += "." + value.slice(16, 24);
+  octeto += "." + value.slice(24, 32);
+  return octeto;
 }
